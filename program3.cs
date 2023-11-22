00be2a22dfb9670be782fdfa1c721750a2ae904a
reena2000 <151727713+reena2000@users.noreply.github.com>
2023-11-22 15:24:45 +0530

    Add files via upload

---
+++ program3.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the height of the triangle:");
+        if (int.TryParse(Console.ReadLine(), out int height))
+        {
+            PrintTriangle(height);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+    }
+
+    static void PrintTriangle(int height)
+    {
+        for (int i = 1; i <= height; i++)
+        {
+            // Print spaces before the stars
+            for (int j = 0; j < height - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            // Print stars
+            for (int k = 0; k < 2 * i - 1; k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine(); // Move to the next line after each row
+        }
+    }
+}
